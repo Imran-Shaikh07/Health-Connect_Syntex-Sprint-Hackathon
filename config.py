b04aff236bd5cb88f3bb233d5178edf2a1cd8d93
Shaikh Imran Nizamuddin <158813283+Imran-Shaikh07@users.noreply.github.com>
2025-02-21 18:32:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+FIREBASE_CONFIG = {
+  "type": "service_account",
+  "project_id": "healthcare-c1980",
+  "private_key_id": "7374b3e408b5401b531b606455a9975bcbfbcc24",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCK8IfojETtDCFR\ncrrkujQrwXhTCH7SxK0piuZC3dqPcgl0Bqt4fYdV1yTOKguprz7VBQED71y69Yqx\nwrTBMYr6TRbqRZRh6Uy59FnvS/U6/X/5E9MscrG/etnmNM3bU3zUmYazaSi+mQJR\nphNLTYd2Inq6RCNxlzvISeJhHwPH20Oeynn8uK+VdolzJapVQnUHwmM0IsVVghY0\nUx38RtSmt5/151rK4uKOZ1Z5mn1ZrPchY9uLgXq/2V/P20izg2iTN47ZSxC2McD7\nu/ud1BxmPS40pm0HJupLXtponPPYTegJPo9rHsPqEfU66ZhG1fPxA1oQzTN8DW7W\nHoH9RoAVAgMBAAECggEABVwB4haHEh3Fqz6htvD30jhES++n9LlhdJjHCQ1FwQLF\nGpSnSJ6O+eCyqsE0cfcwlUb8Iwq4qP2PvAumrU0b70vJ9n94mc5GB2ZFkdV8QCpH\nJXR+UNibwqcagKbIyg0OY6UIbSNBTxjX3g5p8B2tfAEYetbmBxuuUquG7/giTTM5\ne7PRdl2CtU2efVVG2jrcj+QXPcYQbrqM558BWj9jO4lZpkekmMqRCIUXQkYGfwHl\nzl0MCSqKorIXv0h7k7ZSXH1lyXwLsDjJFF6qhy3fhwRFdQeemAo1U30E4/cKAMoF\nqNWyIXKm/xoEVTigqxqWWJUEOjFGiS44UWr9TPWMAQKBgQC9jqGvA9nQa/RRhXPN\nejxGBq3eNQ5D9F6I+42qdFa68e0pzv/MwBuMXdVIkoXPRWtcNLQJVPmni5OY7ffl\npVrF2MZRU0Wl3F1YvawhAYdlL5BvczagEOE3SoZ/YrQpePyhEuyGxPsa8ZVt1dTJ\njxM+gvIpMEMWn8zzuFYr/9xlWQKBgQC7o+ArJ/ZBtnAC+Ke3o8MQwDQzy4E+drXR\nDXBKFnH50K5kXdy7XX8M9z3S0+mqVDdlbgSkN3wZJ6RQdu2hAp/rWt8v3VRJNBxg\nbeKZNV96xN9Uf01oinG2eiBrID7epJSsv7PCQTMB6C5or+u0angHve3eUwUtn9Im\n5lvooMWNHQKBgQCpJI+zap0+dEXzREC79jm4Rpd4syEClBiS5NxZYCxP7jYw20u3\n+SfbzZLWiiAqfog5I7IOrauDzHEswhM08KKZ7j+wK7z3LjaBS+Y/IW0eZh3cxRLW\nK5m7S53OJ1YZt6K9W/SAoV5rgWxUML0KCcgjPJc3F6CVnIw2YaNK6alRKQKBgEzi\n22oTl95ZSRCOnvqqibWFbF6E1tKnhdzBau+QvuckUTEjHN1RdEv1y0lmN+rpU0Gm\nkRRQ//K2waeO89Pz5V9k/ur4AB920OHaZO1uZ09A+FoBR9C8WdfxtvLuZZSJT7px\nWtEQMjxnzQxseWKV9dUGSD+/OPH2+P+jiKmLpycpAoGAdEAIQOU38yTVgo34Fxz/\n2AVg8cA3ZMizReRxU+sYyt9ApJClnOv98YNfPdrVnHf0n6VXteYthSwJlL3Th4RR\nfEu0pMrd0HA2KRk9ceYgl15n+Sn7RjBTNdUWURSNDObvb6tZ4QmzKr4MBzcOiB+D\nQbhK5/97TJVlf7waguM4HLw=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@healthcare-c1980.iam.gserviceaccount.com",
+  "client_id": "111541365081051780280",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40healthcare-c1980.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+
